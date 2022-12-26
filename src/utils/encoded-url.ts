3785e825836ab11d2a7d8ae4585066aa8b1aa8c3
14ROVI <29734170+14ROVI@users.noreply.github.com>
2022-12-26 22:58:48 +0000

    support amplify_video twitter url

--- src/utils/encoded-url.ts
+++ src/utils/encoded-url.ts
@@ -1,39 +1,47 @@
 const ALPHA_NUMS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
 function B64encode(value: number | bigint) {
     let big_value = BigInt(value);
-    let result = '', mod;
+    let result = '';
     do {
-        mod = big_value % 64n;
+        let mod = big_value % 64n;
         result = ALPHA_NUMS.charAt(Number(mod)) + result;
         big_value = big_value / 64n;
     } while (big_value > 0);
 
     return result;
-};
+}
 
 function B64decode(value: string) {
     let result = 0n;
-    for (var i = 0, len = value.length; i < len; i++) {
+    for (let i = 0, len = value.length; i < len; i++) {
         result *= 64n;
         result += BigInt(ALPHA_NUMS.indexOf(value[i]));
     }
 
     return result;
-};
+}
 
 export function encodeUrl(url: string): string {
-    const re = /\/ext_tw_video\/(?<tweet_id>\d+)\/pu\/vid\/(?<width>\d+)x(?<height>\d+)\/(?<video_id>.*).mp4/g;
+    const extRe = /\/ext_tw_video\/(?<tweet_id>\d+)\/pu\/vid\/(?<width>\d+)x(?<height>\d+)\/(?<video_id>.*).mp4/g;
+    const amplifyRe = /\/amplify_video\/(?<tweet_id>\d+)\/vid\/(?<width>\d+)x(?<height>\d+)\/(?<video_id>.*).mp4/g;
 
-    const matches = url.matchAll(re);
-    
-    for (const match of matches) {
+    for (const match of url.matchAll(extRe)) {
         if (match.groups) {
-            console.log(match.groups);
             let tweet_id = B64encode(BigInt(match.groups.tweet_id));
             let width = B64encode(parseInt(match.groups.width));
             let height = B64encode(parseInt(match.groups.height));
             let video_id = match.groups.video_id;
-            return `https://vst.rovi.me/${tweet_id}/${width}/${height}/${video_id}.mp4`;
+            return `https://vst.rovi.me/e/${tweet_id}/${width}/${height}/${video_id}.mp4`;
+        }
+    }
+
+    for (const match of url.matchAll(amplifyRe)) {
+        if (match.groups) {
+            let tweet_id = B64encode(BigInt(match.groups.tweet_id));
+            let width = B64encode(parseInt(match.groups.width));
+            let height = B64encode(parseInt(match.groups.height));
+            let video_id = match.groups.video_id;
+            return `https://vst.rovi.me/a/${tweet_id}/${width}/${height}/${video_id}.mp4`;
         }
     }
 
@@ -41,18 +49,20 @@ export function encodeUrl(url: string): string {
 }
 
 export function decodeUrl(url: string): string | undefined {
-    const re = /\/(?<tweet_id>[a-zA-Z0-9\-_]+)\/(?<width>[a-zA-Z0-9\-_]+)\/(?<height>[a-zA-Z0-9\-_]+)\/(?<video_id>.*).mp4/g;
+    const re = /(?:\/(?<type>[ae]))?\/(?<tweet_id>[a-zA-Z0-9\-_]+)\/(?<width>[a-zA-Z0-9\-_]+)\/(?<height>[a-zA-Z0-9\-_]+)\/(?<video_id>.*).mp4/g;
 
-    const matches = url.matchAll(re);
-    
-    for (const match of matches) {
+    for (const match of url.matchAll(re)) {
         if (match.groups) {
-            console.log(match.groups);
+            let type = match.groups.type;
             let tweet_id = B64decode(match.groups.tweet_id);
             let width = B64decode(match.groups.width);
             let height = B64decode(match.groups.height);
             let video_id = match.groups.video_id;
-            return `https://video.twimg.com/ext_tw_video/${tweet_id}/pu/vid/${width}x${height}/${video_id}.mp4`;
+            if (type === "e" || type === undefined) {
+                return `https://video.twimg.com/ext_tw_video/${tweet_id}/pu/vid/${width}x${height}/${video_id}.mp4`;
+            } else if (type === "a") {
+                return `https://video.twimg.com/amplify_video/${tweet_id}/vid/${width}x${height}/${video_id}.mp4`;
+            }
         }
     }
 
